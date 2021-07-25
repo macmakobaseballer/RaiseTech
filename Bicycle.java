137398ea1d4d97e1afeec5d91a360ee70a05209a
macmakobaseballer <83934720+macmakobaseballer@users.noreply.github.com>
2021-07-25 14:00:42 +0900

    Add files via upload

---
+++ Bicycle.java
@@ -0,0 +1,16 @@
+package macspace;
+
+public class Bicycle {
+	  
+	  private String name ;
+	  
+	  public Bicycle(String nm){
+	    this.name = nm ;
+	  }  
+
+	public  String getName(){
+	    return this.name;
+
+	  }
+	  
+	}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package macspace;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	  Bicycle bianchi = new Bicycle("ビアンキ");
+	    
+	  System.out.println("【自転車の情報】");
+	  System.out.println("名前：" + bianchi.getName()) ;
+	  
+	}
+
+}
